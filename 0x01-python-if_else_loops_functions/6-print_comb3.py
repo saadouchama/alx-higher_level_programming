7554622693acac61391dde6c6c90451fcfc34a59
Saad Ouchama <saadochama@gmail.com>
2023-06-06 04:42:59 -0700

    add task 7: 6-print_comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+i = 0
+j = 0
+while i < 9:
+    j = i + 1
+    while j < 10:
+        print("{:s}{:s}".format(str(i), str(j)), end='')
+        if i < 8:
+            print(end=', ')
+        j += 1
+    i += 1
+print("\n")
