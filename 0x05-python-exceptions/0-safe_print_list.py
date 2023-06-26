c3a5a3fbac69a2910e236df3b3a2c35ec2a00b23
Saad Ouchama <saadochama@gmail.com>
2023-06-26 14:51:25 -0700

    fix last task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,3 +10,14 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return count
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
